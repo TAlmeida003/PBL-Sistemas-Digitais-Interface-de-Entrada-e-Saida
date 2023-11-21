6601fe604c4c0673ac0e85735589bb2cc9ba5fc2
Silvio <silviozv.oli@gmail.com>
2023-11-20 21:40:37 -0300

    feat(UART Assembly): atualizando arquivo de teste.

--- Fase 2 - Interface de Teste em Assembly/teste2_uart.s
+++ Fase 2 - Interface de Teste em Assembly/teste2_uart.s
@@ -2,19 +2,23 @@
 .EQU    sys_open,               5           @ Número do serviço Linux para abrir um arquivo
 .EQU    sys_mmap2,              192         @ Número do serviço Linux para mapear memória
 
-.EQU UART_THR,          0x0000   @ Dado a ser transmitido na UART 
-.EQU UART_DLL,          0x0000   @ 8 bits mais baixos do divisor de baud rate (7:0)
-.EQU UART_DLH,          0x0004   @ 8 bits mais altos do divisor de baud rate (7:0)
+.EQU UART_THR,                  0x0000      @ Dado a ser transmitido na UART 
+.EQU UART_DLL,                  0x0000      @ 8 bits mais baixos do divisor de baud rate (7:0)
+.EQU UART_DLH,                  0x0004      @ 8 bits mais altos do divisor de baud rate (7:0)
 
-.EQU UART_FCR,          0x0008   @ Registrador de controle dos FIFOs
-.EQU UART_FIFOE,        0b1   @ Habilita os FIFOs
+.EQU UART_FCR,                  0x0008      @ Registrador de controle dos FIFOs
+.EQU UART_FIFOE,                0b1         @ Habilita os FIFOs
 
-.EQU UART_LCR,          0x000C   @ Registrador de linhas de controle
-.EQU UART_DLAB_TR ,     0x00000000   @ Seta para receber/enviar dados (7 << 0)
-.EQU UART_DLAB_BD ,     0b10000000   @ Seta o baud rate (7 << 1)
-.equ UART_DLS,          0b11    @ Tamanho do cojunto de bits da UART (11 - 8 bits)
+.EQU UART_LCR,                  0x000C      @ Registrador de linhas de controle
+.EQU UART_DLAB_TR ,             0x00000000  @ Seta para receber/enviar dados (7 << 0)
+.EQU UART_DLAB_BD ,             0b10000000  @ Seta o baud rate (7 << 1)
+.equ UART_DLS,                  0b11        @ Tamanho do cojunto de bits da UART (11 - 8 bits)
 
-.EQU sys_nanosleep,     162
+.EQU UART_HALT,                 0x00A4
+.EQU UART_CHANGE_UPDATE,        0b100      
+.EQU UART_CHCFG_AT_BUSY,        0b10      
+
+.EQU sys_nanosleep,             162
 
 .macro UART_Mapeamento
 
@@ -30,7 +34,7 @@
     ldr     r5,     =base_uart          @ endereço UART / 4096
     ldr     r5,     [r5]                @ carrega o endereço
 
-    ldr     R1,     =pagelenn 
+    ldr     R1,     =pagelenn
     ldr     R1,     [R1]
 
     mov     R2,     #3                  @ (PROT_READ + PROT_WRITE) @ opções de proteção de memória
@@ -49,6 +53,9 @@
     mov r0, #UART_DLAB_BD         @ Setando os espaços de endereço para carregar o baud rate
 	str r0, [r9, #UART_LCR]   
 
+    mov r0, #UART_CHCFG_AT_BUSY         @ Habilitando alteração na setagem de baud rate
+	str r0, [r9, #UART_HALT] 
+
     mov r0, #0x45                @ Setando 8 bits baixos do baud rate
 	str r0, [r9, #UART_DLL]    
 
@@ -60,8 +67,19 @@
 	mov r7, #sys_nanosleep       @ Contando o tempo de 1 segundo para carregar o baud rate
 	svc 0
 
-    mov r0, #UART_DLAB_TR         @ Setando os espaços de endereço para carregar dados de transmissão e recebimento
+    mov r0, #UART_CHANGE_UPDATE  @ Carregando alterações
+	str r0, [r9, #UART_HALT] 
+
+    mov r0, #0x01                @ Tempo de 1 segundos
+	mov r1, #0x00
+	mov r7, #sys_nanosleep       @ Contando o tempo de 1 segundo para carregar o baud rate
+	svc 0
+
+    mov r0, #UART_DLAB_TR        @ Setando os espaços de endereço para carregar dados de transmissão e recebimento
 	str r0, [r9, #UART_LCR]     
+
+    mov r0, #0                   @ Desabilitando alteração na setagem de baud rate
+	str r0, [r9, #UART_HALT] 
 
     mov r0, #UART_DLS             @ Setando o tamanho do conjunto de bits lidos pela UART
 	str r0, [r9, #UART_LCR]   
